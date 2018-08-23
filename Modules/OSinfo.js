13777d5cf7ca24c793b3de2a2116582fb443bad7
FrranzMaurer <38557647+FrranzMaurer@users.noreply.github.com>
2018-08-23 18:41:17 +0200

    Add files via upload

--- Modules/OSinfo.js
+++ Modules/OSinfo.js
@@ -12,10 +12,11 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
+    var convTime = convertTime.print(uptime);
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('Uptime: ~', convTime);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
--- Modules/timeconv.js
+++ Modules/timeconv.js
@@ -1,10 +1,5 @@
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-      switch (input) {
-          case (input < 3600):
-          console.log('Minutes: ~', (input / 60).toFixed(0), 'min', (input % 60), 'sec');
-          break;
-          default:
-          console.log("Hours: ~", (input / 3600).toFixed(0), "hrs", ((input % 3600) / 60).toFixed(0), "min", input % 60, "sec");
-        }
-});
+function convertTime (input) {
+  return (input / 3600).toFixed(0), "hrs", ((input % 3600) / 60).toFixed(0), "min", input % 60, "sec";
+}
+
+module.exports = convertTime;
